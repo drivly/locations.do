c770e5baaa987217af514bc2efd29ceb28c4b9ea
Nathan Clevenger <nateclev@gmail.com>
2022-08-27 08:27:15 -0500

    Update worker.js

--- worker.js
+++ worker.js
@@ -6,8 +6,13 @@ export class Location {
   constructor(state, env) {
     this.state = state
     this.state.blockConcurrencyWhile(async () => {
-      this.cf = await fetch('https://workers.cloudflare.com/cf.json').then(res => res.json())
-      this.locations = await fetch('https://speed.cloudflare.com/locations').then(res => res.json())
+      const [cf, locations] = await Promise.all([
+       fetch('https://workers.cloudflare.com/cf.json').then(res => res.json())
+       fetch('https://speed.cloudflare.com/locations').then(res => res.json())
+      ])
+      this.cf = cf
+      this.locations = locations
+      this.objects = await env.LOCATIONS.get(env.LOCATIONS.idFromName('index')).fetch('https://locations.do/' + cf.colo)
     })
   }
   async fetch(req) {
@@ -25,7 +30,7 @@ export class Location {
     }
     return new Response(JSON.stringify({ 
       api,
-      logged,
+      locations: this.objects,
     }, null, 2), { headers: { 'content-type': 'application/json; charset=utf-8' } })
   }
 }
@@ -34,12 +39,19 @@ export class Locations {
   constructor(state, env) {
     this.state = state
     this.state.blockConcurrencyWhile(async () => {
-      this.locations = await this.state.storage.list().then(list => Object.fromEntries(list))
+      this.objects = await this.state.storage.list().then(list => Object.fromEntries(list))
+      this.locations = await fetch('https://speed.cloudflare.com/locations').then(res => res.json())
     })
   }
   async fetch(req) {
     const { origin, hostname, pathname } = new URL(req.url)
-    const [_,location,id] = pathname.split('/')
+    const [_,colo] = pathname.split('/')
+    if (!this.objects[colo]) {
+      let location = this.locations.find(loc => loc.iata == colo)
+      location.url = 'https://locations.do/' + colo
+      this.objects[colo] = location
+      this.state.storage.put(colo, location)
+    } 
     return new Response(JSON.stringify({ 
       api,
       logged,
