2d7fc4d00b710bae2f08a1d3953f5aada5ad37e8
Nathan Clevenger <nateclev@gmail.com>
2022-08-27 08:12:10 -0500

    Update worker.js

--- worker.js
+++ worker.js
@@ -33,9 +33,13 @@ export class Location {
 export class Locations {
   constructor(state, env) {
     this.state = state
+    this.state.blockConcurrencyWhile(async () => {
+      this.locations = await this.state.storage.list().then(list => Object.fromEntries(list))
+    })
   }
   async fetch(req) {
-    const { origin, hostname, pathname, search, searchParams } = new URL(req.url)
+    const { origin, hostname, pathname } = new URL(req.url)
+    const [_,location,id] = pathname.split('/')
     return new Response(JSON.stringify({ 
       api,
       logged,
